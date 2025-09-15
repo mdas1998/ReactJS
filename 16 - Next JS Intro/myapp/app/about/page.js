9ef69692cf6511d551f215f49bd3393965b51fde
Micheal Das <michealdas1998@gmail.com>
2025-09-15 13:15:45 +0530

    Redux and introduction to NextJS

---
+++ 16 - Next JS Intro/myapp/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-gray-800 text-white p-4 my-4'>
+      <h1 className='text-xl'>I am a about page</h1>
+      <ul className='my-4'>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Next Router from Next Navigation</li>
+        <li>Optimized Rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
